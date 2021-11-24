3e69697f5e2cfb02ae9db67c3d470e9b31d74ddd
Gabi-Andrade <gabrielandradetuc@gmail.com>
2021-11-24 16:08:12 -0300

    Inicio de sesion agregada a moduloespacio.h

--- moduloespacios.h
+++ moduloespacios.h
@@ -1,6 +1,6 @@
 void Espacios()
 {
-	int op;
+	int op,x;
 	do
 	{
 		system("cls");
@@ -18,17 +18,31 @@ void Espacios()
 			case 1:
 				printf("\tInicio de Secion\n");
 				printf("\t==================\n\n");
-				
+				x=IniciarSesion();
 				break;
 			case 2:
 				printf("\tLista de Espara (informe)\n");
 				printf("\t===========================\n\n");
-				
+				if(x==1)
+				{
+					Lista();
+				}
+				else
+				{
+					printf("Debe iniciar sesion");
+				}
 				break;
 			case 3:
 				printf("\tRegistro de Tratamiento\n");
 				printf("\t=========================\n\n");
-				
+				if(x==1)
+				{
+					Tratamiento();
+				}
+				else
+				{
+					printf("Debe iniciar sesion");
+				}
 				break;
 			case 4:
 				printf("\t****************************************\n");
@@ -37,10 +51,54 @@ void Espacios()
 				break;
 			default:
 				printf("\t**ERROR**\n\nDebe ingresar una opcion valida");
-				getch();
 		}
+		getch();
 	}
 	while (op!=4);
+}
+int IniciarSecion()
+{
+	int ban=0,u=0,c=0;
+	char user[10], cont[10];
+	FILE *arch;
+	arch=fopen("Profesionales.dat","rb");
+	
+	if(arch==NULL)
+	{
+		printf("Debe solicitar en administracion un ususario y una contraseña");
+	}
+	else
+	{
+		printf("Usuario: ");
+		_flushall();
+		gets(user);
+		fread(&reg,sizeof(Registro),1,arch);
+		while(!feof(arch) && ban==0)
+		{
+			if(strcmp(user,reg.user)==0)
+			{
+				u=1;
+				printf("Contraseña: ");
+				_flushall();
+				gets(cont);
+				if(strcmp(cont,reg.cont)==0)
+				{
+					ban=1;
+					c=1;
+				}
+			}
+		}
+	}
+	if(u==0)
+	{
+		printf("Usuario incorrecto");
+	}
+	if(c==0)
+	{
+		printf("Contraseña incorrecta");
+	}
+	getch();
+	return ban;
 }
 
 
