60d1ba3616db344c31549a61d70ef2bca7c00460
Gabi-Andrade <gabrielandradetuc@gmail.com>
2021-12-04 13:09:27 -0300

    estructuras completas

--- estructuras.h
+++ estructuras.h
@@ -1,7 +1,7 @@
 struct Fecha
 {
 	int dd,mm,aa;
-}
+};
 struct Usuarios
 {
 	char Usua[10], Cont[10], ApeNom[10];
@@ -11,9 +11,9 @@ struct Profesionales
 	char ApeNom[60],Tel[25];
 	int IDProf,DNI;
 };
-struct Cliente
+struct Clientes
 {
-	char ApeNom[60], Dom[60], Loc[60], Tel[25];
+	char ApeNom[60], Dom[60], Loc[60], Tel[25], Trat[380];
 	int DNICliente;
 	Fecha FecNac;
 	float Peso;
